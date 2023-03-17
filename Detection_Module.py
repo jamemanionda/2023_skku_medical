465839d1ca2cd51029d8282341188a12ce8d98ff
qrwgs99 <qrwgs99@naver.com>
2023-03-17 10:45:16 +0900

    Update Detection_Module.py

--- Detection_Module.py
+++ Detection_Module.py
@@ -1,6 +1,4 @@
 import UI_test
-a = UI_test.Dicom_Information()
-b = UI_test.Dicom_Information()
 
 #위변조 탐지
 class DetectionModule():
@@ -8,7 +6,7 @@ class DetectionModule():
     def __init__(self):
         self.diff_vars = []
 
-    #원본과 대조, 다른 값이 있다면 딕셔너리에 추가
+    #원본과 대조, 다른 값이 있다면 추가
     def compare(self, a, b):
         for key, value in a.__dict__.items():
             if value != b.__dict__[key]:
@@ -24,5 +22,7 @@ class DetectionModule():
             print("위조 추정 위치 : ", self.diff_vars)
 
 #테스트
+a = UI_test.Dicom_Information()
+b = UI_test.Dicom_Information()
 c = DetectionModule()
 c.compare(a, b)
