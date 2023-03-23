db78a78eec66d2dee1ec70beb04b434269ae7cff
qrwgs99 <qrwgs99@naver.com>
2023-03-23 12:02:09 +0900

    Update Log_Timeline.py

--- Log_Timeline.py
+++ Log_Timeline.py
@@ -10,13 +10,14 @@ class LogTimeline:
         self.logfile_path = ''
         self.logdf = pd.DataFrame()
         self.visualdf = pd.DataFrame()
-
+    """
+    #PACSSERVER logfile
     #로그 파일 불러오기
     def input_logfile(self):
         #self.logfile_path = input("logfile path: ")
-        self.logfile_path = 'PACSSERVER-20230315-0743.log'
-        self.logdf = pd.read_table(self.logfile_path, sep='\t', encoding='UTF-16')
-        self.logdf['Timestamp'] = pd.to_datetime(self.logdf['Timestamp'])
+        self.logfile_path = 'PACSSERVER-20230315-0743.log'      #로그 파일 경로 입력
+        self.logdf = pd.read_table(self.logfile_path, sep='\t', encoding='UTF-16')      #DataFrame 생성
+        self.logdf['Timestamp'] = pd.to_datetime(self.logdf['Timestamp'])       #'Timestamp'를 날짜값으로 변경
 
     #로그 파일 시각화
     def visualization(self):
@@ -29,6 +30,33 @@ class LogTimeline:
         self.visualdf.plot(kind='bar')                                  #바 형태의 그래프로 시각화
         plt.xlabel("Timestamp")
         plt.ylabel("Frequency")
+        plt.show()
+    """
+
+    def input_logfile(self):
+        #self.logfile_path = input("logfile path: ")
+        self.logfile_path = '221209_HISTORY_test.xls'
+        self.logdf = pd.read_excel(self.logfile_path)
+        self.logdf['ChangeDate'] = pd.to_datetime(self.logdf['ChangeDate'])
+
+        # 로그 파일 시각화
+    def visualization(self):
+        tmpdf = self.logdf.loc[self.logdf['ModifyType'].isin(['0', '2', '3'])]
+        self.visualdf = tmpdf[['ChangeDate', 'ModifyType']]  # timestamp와 행위기록만 추출
+        self.visualdf['ChangeDate'] = pd.to_datetime(self.visualdf['ChangeDate'])
+
+        grouped = self.visualdf.groupby(['ChangeDate', 'ModifyType']).size().unstack(fill_value=0)
+        grouped = grouped.reset_index('ChangeDate')
+
+        counts = grouped.groupby(pd.Grouper(key='ChangeDate', freq='H')).sum()
+        # Plot the results
+        counts.plot(kind='bar')
+        # Add axis labels and title
+        plt.xlabel('Hour')
+        plt.ylabel('Count')
+        plt.title('Counts of ModifyType values per hour')
+        plt.yticks(np.arange(0,10,1))
+        # 그래프 그리기
         plt.show()
 
 #테스트
