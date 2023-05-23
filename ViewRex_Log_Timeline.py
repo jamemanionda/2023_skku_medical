bd7830708903cddb6fcc4713f3d966e97e1753e1
qrwgs99 <qrwgs99@naver.com>
2023-05-23 16:38:32 +0900

    make table and export to excel

--- ViewRex_Log_Timeline.py
+++ ViewRex_Log_Timeline.py
@@ -3,23 +3,22 @@ import matplotlib.pyplot as plt
 from matplotlib.ticker import MultipleLocator
 import pandas as pd
 import numpy as np
-import datetime
+from datetime import datetime
 import glob
 import re
 import os
 import getpass
+from tabulate import tabulate
+import socket
 
-#ViewRex 로그 파일 타임라인 제작
-class ViewRexLogTimeline():
+class ViewRexLogDataFrame():
     def __init__(self):
         self.viewrexlogfile_folder = ''
-        self.viewrexdf_modify = pd.DataFrame(columns=['Time'])#, 'Number', 'Action'])
-        self.viewrexdf_select = pd.DataFrame(columns=['Time'])#, 'Number', 'Action'])
-        self.viewrexdf_export = pd.DataFrame(columns=['Time'])#, 'Number', 'Action'])
-        self.mindex = 0
-        self.sindex = 0
-        self.eindex = 0
+        self.viewrexlog_dataframe = pd.DataFrame(columns=['Computer Name', 'IP Address', 'Time', 'Action'])
+        self.index = 0
         self.viewrexlogfile_folder = 'C:\\TechHeim\\ViewRex3\\Log'      #로그 파일이 저장되는 기본 경로
+        self.computer_name = socket.gethostname()
+        self.computer_ip = socket.gethostbyname(self.computer_name)
     #'C:\\'+'컴퓨터이름'+''
     #모든 ViewRex 로그 파일을 불러옴
     def input_viewrexlogfile(self):
@@ -27,6 +26,7 @@ class ViewRexLogTimeline():
             self.viewrexlogfile_folder = 'C:\\Users\\skku-dfl\\OneDrive - 성균관대학교\\문서\\SKKU_DFL\\DICOM\\Log'    #실제 로그파일의 폴더를 받을 수 있도록 변경할 것
         self.viewrexlogfile_path = self.viewrexlogfile_folder + "\\ViewRex.exe_*.log"
         for logfile in glob.glob(self.viewrexlogfile_path):
+            filename = os.path.basename(logfile)
             with open(logfile, 'r', encoding='UTF-8') as file:
                 for line in file:
                     #if line == '\n':
@@ -45,42 +45,48 @@ class ViewRexLogTimeline():
                         Action = text[4]
 
                     if 'Modify Dicom infomation' in line:
-                        self.viewrexdf_modify.loc[self.mindex] = [Time]#, modifyNumber, modifyAction]
-                        self.mindex += 1
+                        self.viewrexlog_dataframe.loc[self.index] = [self.computer_name, self.computer_ip, Time, 'Modify']
+                        self.index += 1
 
                     elif 'FROM StudyInformation' in line:
-                        self.viewrexdf_select.loc[self.sindex] = [Time]#, selectNumber, selectAction]
-                        self.sindex += 1
+                        self.viewrexlog_dataframe.loc[self.index] = [self.computer_name, self.computer_ip, Time, 'Select']
+                        self.index += 1
 
                     elif 'FROM Patient' in line:
-                        self.viewrexdf_select.loc[self.sindex] = [Time]  # , selectNumber, selectAction]
-                        self.sindex += 1
+                        self.viewrexlog_dataframe.loc[self.index] = [self.computer_name, self.computer_ip, Time, 'Select']
+                        self.index += 1
 
                     elif 'Export File Path' in line:
-                        self.viewrexdf_export.loc[self.eindex] = [Time]#, exportNumber, exportAction]
-                        self.eindex += 1
+                        self.viewrexlog_dataframe.loc[self.index] = [self.computer_name, self.computer_ip, Time, 'Export']
+                        self.index += 1
 
-        #pd.set_option('display.max_columns', None)
-        #pd.set_option('display.max_rows', None)
-        #print(self.viewrexdf_select)
-        self.viewrexdf_modify['Time'] = pd.to_datetime(self.viewrexdf_modify['Time'])
-        self.viewrexdf_select['Time'] = pd.to_datetime(self.viewrexdf_select['Time'])
-        self.viewrexdf_export['Time'] = pd.to_datetime(self.viewrexdf_export['Time'])
-        tmpmdf = self.viewrexdf_modify['Time'].value_counts()
-        tmpsdf = self.viewrexdf_select['Time'].value_counts()
-        tmpedf = self.viewrexdf_export['Time'].value_counts()
-        #tmpdf = tmpdf.reset_index('Time')
+        self.viewrexlog_dataframe['Time'] = pd.to_datetime(self.viewrexlog_dataframe['Time'])
+        self.viewrexlog_df = self.viewrexlog_dataframe.groupby([pd.Grouper(key='Time', freq='D'), 'Action'])['Action'].agg(['count'])
+        pd.set_option('display.max_columns', None)
+        pd.set_option('display.max_rows', None)
+        #print(self.viewrexlog_dataframe)
 
-        #print(tmpmdf)
+        print(self.viewrexlog_df)
+        #tmpsdf = tmpsdf.reset_index('Time')
 
-        tmpmdf.plot(kind='line', figsize=(12, 7), color='Red')
-        tmpsdf.plot(kind='line', figsize=(12, 7), color='Green')
-        tmpedf.plot(kind='line', figsize=(12, 7), color='Blue')
-        plt.xlabel('Time')
-        plt.ylabel('Count')
-        plt.show()
+        #testcount = self.viewrexdf_select.resample('D', on='Time').count()
+        #print(testcount)
 
+    #excel로 추출
+    def export_log_to_excel(self):
+        file_names = os.listdir()
+        today = datetime.today().strftime("%Y%m%d")
+        viewrexlog_filename = 'ViewRexLog_' + str(today)
+        tempviewrex_filename = viewrexlog_filename
+        filenum = 2
+        while True:
+            if tempviewrex_filename + '.xlsx' not in file_names:
+                self.viewrexlog_df.to_excel(tempviewrex_filename + '.xlsx', encoding='cp949')
+                break
+            else:
+                tempviewrex_filename = viewrexlog_filename + '(' + str(filenum) + ')'
+                filenum += 1
 
-
-test = ViewRexLogTimeline()
+test = ViewRexLogDataFrame()
 test.input_viewrexlogfile()
+test.export_log_to_excel()
